954527f431e0de9fae041827feb9e4d00f4cd7a3
Marco Facheris <marco@imbeard.com>
2016-07-29 17:18:59 +0200

    upgrades 2

--- search.php
+++ search.php
@@ -5,10 +5,10 @@
 
 get_header(); ?>
 
-	<?php while ( have_posts() ) : the_post(); ?>
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 		<?php the_content(); ?>
 
-	<?php endwhile; ?>
+	<?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
